b24376657e72089e6f596346e173c88a4e79adbf
Kiril Tsvetanov <kiril.tsvetanov@geniussports.com>
2022-09-19 16:16:53 +0300

    adasd

--- project1202/index.php
+++ project1202/index.php
@@ -1,5 +1,10 @@
 <?php
-
+$development = true;
+if ($development) {
+    // Show Queries only
+    header("Content-Type: application/json");
+    $return = [];
+}
 // 1. DB Connection
 try {
     $con = new PDO('mysql:host=db;dbname=TESTWHAMERICANFOOTBALL;charset=utf8', 'root', '12345');
@@ -9,8 +14,8 @@ try {
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 
-// Help function
-function get_string_between($string, $start, $end)
+// Helper function
+function getStringBetween($string, $start, $end)
 {
     $string = ' ' . $string;
     $ini = strpos($string, $start);
@@ -22,23 +27,26 @@ function get_string_between($string, $start, $end)
 
 // 2. Get all matchIds from football_match_actions_competitionId_11.txt
 //     FORMAT
-//    $football_match_actions_competitionId_11 = [
+//    $footballMatchActionsCompetitionId11 = [
 //        'matchId_1',
 //        'matchId_2',
 //    ];
-$football_match_actions_competitionId_11 = [];
-$fileinput = "football_match_actions_competitionId_11.txt";
-$file = fopen($fileinput, "r");
+$footballMatchActionsCompetitionId11 = [];
+$fileInput = "football_match_actions_competitionId_11.txt";
+$file = fopen($fileInput, "r");
 while (!feof($file)) {
     $data = fgets($file);
     $matchId = trim($data);
-    $football_match_actions_competitionId_11[] = $matchId;
+    $footballMatchActionsCompetitionId11[] = $matchId;
 }
 fclose($file);
+if ($development) {
+    $return['football_match_actions_competitionId_11'] = $footballMatchActionsCompetitionId11;
+}
 
 // 3. Get all matchIds from player_resolve.txt
 //    FORMAT
-//    $player_resolve = [
+//    $playerResolve = [
 //        'matchId_1' => [
 //            'temporaryId_1' => 'personId_1',
 //            'temporaryId_2' => 'personId_2',
@@ -47,23 +55,25 @@ fclose($file);
 //            'temporaryId_3' => 'personId_3',
 //        ],
 //    ];
-$player_resolve = [];
-$fileinput = "player_resolve.txt";
-$file = fopen($fileinput, "r");
+$playerResolve = [];
+$fileInput = "player_resolve.txt";
+$file = fopen($fileInput, "r");
 while (!feof($file)) {
     $data = fgets($file);
     $parts = explode('-----', $data);
     $url = $parts[0];
-    $matchId = get_string_between($url, '/matches/', '/persons/');
+    $matchId = getStringBetween($url, '/matches/', '/persons/');
     $json = $parts[1];
     $jsonDecode = json_decode($json, true);
-    $player_resolve[$matchId][$jsonDecode['temporaryId']] = $jsonDecode['personId'];
+    $playerResolve[$matchId][$jsonDecode['temporaryId']] = $jsonDecode['personId'];
 }
 fclose($file);
-
-// 4. Look for concurrence in $football_match_actions_competitionId_11 and $player_resolve
+if ($development) {
+    $return['player_resolve'] = $playerResolve;
+}
+// 4. Look for concurrence in $footballMatchActionsCompetitionId11 and $playerResolve
 //     FORMAT
-//    $final = [
+//    $concurrence = [
 //        'matchId_1' => [
 //            'temporaryId_1' => 'personId_1',
 //            'temporaryId_2' => 'personId_2',
@@ -72,55 +82,51 @@ fclose($file);
 //            'temporaryId_3' => 'personId_3',
 //        ]
 //    ];
-$final = [];
-foreach ($football_match_actions_competitionId_11 as $competitionId_11_matchId) {
-    foreach ($player_resolve as $player_resolve_matchId => $player_resolve_matchId_data) {
-        if ($competitionId_11_matchId == $player_resolve_matchId) {
-            $final[$competitionId_11_matchId] = $player_resolve_matchId_data;
-        }
-    }
+$concurrence = array_filter(
+    $playerResolve,
+    function ($key) use ($footballMatchActionsCompetitionId11) {
+        return in_array($key, $footballMatchActionsCompetitionId11);
+    },
+    ARRAY_FILTER_USE_KEY
+);
+if ($development) {
+    $return['concurrence'] = $concurrence;
+
+    $concurrence = [
+        1 => [
+            -111111111 => -333333333,
+            -22222222 => -444444444,
+        ]
+    ];
+    $return['testing_concurrence'] = $concurrence;
 }
-
 // 5. Update `match_action` table field 'personId'
-foreach ($final as $matchId => $playersSwap) {
-    $matchId = 1; // TODO KIRIL REMOVE
-    $counter = 0; // TODO KIRIL REMOVE
+foreach ($concurrence as $matchId => $playersSwap) {
     foreach ($playersSwap as $oldPlayedId => $newPlayerId) {
-        $counter++; // TODO KIRIL REMOVE
-        if ($counter == 1) { // TODO KIRIL REMOVE
-            $oldPlayedId = -111111111; // TODO KIRIL REMOVE
-            $newPlayerId = -333333333; // TODO KIRIL REMOVE
-        } // TODO KIRIL REMOVE
-        if ($counter == 2) { // TODO KIRIL REMOVE
-            $oldPlayedId = -22222222; // TODO KIRIL REMOVE
-            $newPlayerId = -444444444; // TODO KIRIL REMOVE
-        } // TODO KIRIL REMOVE
-
         $sql = "UPDATE `match_action` SET `personId` = :newPlayerId WHERE (`matchId` = :matchId) and (`personId` = :oldPlayedId);";
         $qry = $con->prepare($sql);
         $qry->bindParam(':matchId', $matchId);
         $qry->bindParam(':oldPlayedId', $oldPlayedId);
         $qry->bindParam(':newPlayerId', $newPlayerId);
-        $qry->execute();
+        if ($development) {
+            $queryString = [
+                $qry->queryString => [
+                    'query' => $matchId,
+                    'matchId' => $matchId,
+                    'oldPlayedId' => $oldPlayedId,
+                    'newPlayerId' => $newPlayerId,
+                ]
+            ];
+            $return['query_string'][] = $queryString;
+        } else {
+            $qry->execute();
+        }
     }
-    break; // TODO KIRIL REMOVE
 }
 
 // 6. Update `match_action` table field 'playersTeam1'
-foreach ($final as $matchId => $playersSwap) {
-    $matchId = 1; // TODO KIRIL REMOVE
-    $counter = 0; // TODO KIRIL REMOVE
+foreach ($concurrence as $matchId => $playersSwap) {
     foreach ($playersSwap as $oldPlayedId => $newPlayerId) {
-        $counter++; // TODO KIRIL REMOVE
-        if ($counter == 1) { // TODO KIRIL REMOVE
-            $oldPlayedId = -111111111; // TODO KIRIL REMOVE
-            $newPlayerId = -333333333; // TODO KIRIL REMOVE
-        } // TODO KIRIL REMOVE
-        if ($counter == 2) { // TODO KIRIL REMOVE
-            $oldPlayedId = -22222222; // TODO KIRIL REMOVE
-            $newPlayerId = -444444444; // TODO KIRIL REMOVE
-        } // TODO KIRIL REMOVE
-
         $sql = "UPDATE `match_action` SET `playersTeam1` = REPLACE(`playersTeam1`, :oldPlayedId, :newPlayerId) " .
             "WHERE (`matchId` = :matchId) and (`playersTeam1` LIKE :oldPlayedIdLike1 OR `playersTeam1` LIKE :oldPlayedIdLike2)";
         $qry = $con->prepare($sql);
@@ -133,26 +139,22 @@ foreach ($final as $matchId => $playersSwap) {
 
         $oldPlayedIdLike2 = '%' . ';' . $oldPlayedId . ';' . '%';
         $qry->bindParam(':oldPlayedIdLike2', $oldPlayedIdLike2);
+        if ($development) {
+            $queryString = [
+                $qry->queryString => [
+                    'matchId' => $matchId,
+                    'oldPlayedIdLike1' => $oldPlayedIdLike1,
+                    'oldPlayedIdLike2' => $oldPlayedIdLike2,
+                ]
+            ];
+            $return['query_string'][] = $queryString;
+        }
         $qry->execute();
     }
-    break; // TODO KIRIL REMOVE
 }
-
 // 7. Update `match_action` table field 'playersTeam2'
-foreach ($final as $matchId => $playersSwap) {
-    $matchId = 1; // TODO KIRIL REMOVE REMOVE
-    $counter = 0; // TODO KIRIL REMOVE
+foreach ($concurrence as $matchId => $playersSwap) {
     foreach ($playersSwap as $oldPlayedId => $newPlayerId) {
-        $counter++; // TODO KIRIL REMOVE
-        if ($counter == 1) { // TODO KIRIL REMOVE
-            $oldPlayedId = -111111111; // TODO KIRIL REMOVE
-            $newPlayerId = -333333333; // TODO KIRIL REMOVE
-        } // TODO KIRIL REMOVE
-        if ($counter == 2) { // TODO KIRIL REMOVE
-            $oldPlayedId = -22222222; // TODO KIRIL REMOVE
-            $newPlayerId = -444444444; // TODO KIRIL REMOVE
-        } // TODO KIRIL REMOVE
-
         $sql = "UPDATE `match_action` SET `playersTeam2` = REPLACE(`playersTeam2`, :oldPlayedId, :newPlayerId) " .
             "WHERE (`matchId` = :matchId) and (`playersTeam2` LIKE :oldPlayedIdLike1 OR `playersTeam2` LIKE :oldPlayedIdLike2)";
         $qry = $con->prepare($sql);
@@ -165,58 +167,66 @@ foreach ($final as $matchId => $playersSwap) {
 
         $oldPlayedIdLike2 = '%' . ';' . $oldPlayedId . ';' . '%';
         $qry->bindParam(':oldPlayedIdLike2', $oldPlayedIdLike2);
+        if ($development) {
+            $queryString = [
+                $qry->queryString => [
+                    'matchId' => $matchId,
+                    'oldPlayedIdLike1' => $oldPlayedIdLike1,
+                    'oldPlayedIdLike2' => $oldPlayedIdLike2,
+                ]
+            ];
+            $return['query_string'][] = $queryString;
+        }
         $qry->execute();
     }
-    break; // TODO KIRIL REMOVE
 }
-
 // 8. Update `match_action` table field 'qualifiers'
-foreach ($final as $matchId => $playersSwap) {
-    $matchId = 1; // TODO KIRIL REMOVE
 //     Each lookup in `qualifiers`. As an example:
 //         kiki:-111111111;type:kickoff;koko:-111111111;opponenttimeofpossession:0.083333;status:ended
 //         grossyards:48;koko:-333333333;netyards:34;kiki:-333333333;
-    $lookFor = [
-        'kiki',
-        'koko',
-    ];
-
-    foreach ($lookFor as $wordLookup) {
-        $counter = 0; // TODO KIRIL REMOVE
+$qualifierKeys = [
+    'kiki',
+    'koko',
+];
+foreach ($concurrence as $matchId => $playersSwap) {
+    foreach ($qualifierKeys as $qualifierKey) {
         foreach ($playersSwap as $oldPlayedId => $newPlayerId) {
-            $counter++; // TODO KIRIL REMOVE
-            if ($counter == 1) { // TODO KIRIL REMOVE
-                $oldPlayedId = -111111111; // TODO KIRIL REMOVE
-                $newPlayerId = -333333333; // TODO KIRIL REMOVE
-            } // TODO KIRIL REMOVE
-            if ($counter == 2) { // TODO KIRIL REMOVE
-                $oldPlayedId = -22222222; // TODO KIRIL REMOVE
-                $newPlayerId = -444444444; // TODO KIRIL REMOVE
-            } // TODO KIRIL REMOVE
-
             $sql = "UPDATE `match_action` SET `qualifiers` = REPLACE(`qualifiers`, :oldPlayedIdLookup, :newPlayerIdLookup) " .
                 "WHERE (`matchId` = :matchId) and (`qualifiers` LIKE :oldPlayedIdLike1 OR `qualifiers` LIKE :oldPlayedIdLike2)";
             $qry = $con->prepare($sql);
 
             $qry->bindParam(':matchId', $matchId);
 
-            $tmp1 = $wordLookup . ':' . $oldPlayedId . ';';
-            $qry->bindParam(':oldPlayedIdLookup', $tmp1);
+            $oldPlayedIdLookup = $qualifierKey . ':' . $oldPlayedId . ';';
+            $qry->bindParam(':oldPlayedIdLookup', $oldPlayedIdLookup);
 
-            $tmp2= $wordLookup . ':' . $newPlayerId . ';';
-            $qry->bindParam(':newPlayerIdLookup', $tmp2);
+            $newPlayerIdLookup= $qualifierKey . ':' . $newPlayerId . ';';
+            $qry->bindParam(':newPlayerIdLookup', $newPlayerIdLookup);
 
-            $oldPlayedIdLike1 = $wordLookup . ':' . $oldPlayedId . ';' . '%';
+            $oldPlayedIdLike1 = $qualifierKey . ':' . $oldPlayedId . ';' . '%';
             $qry->bindParam(':oldPlayedIdLike1', $oldPlayedIdLike1);
 
-            $oldPlayedIdLike2 = '%' . ';' . $wordLookup . ':' . $oldPlayedId . ';' . '%';
+            $oldPlayedIdLike2 = '%' . ';' . $qualifierKey . ':' . $oldPlayedId . ';' . '%';
             $qry->bindParam(':oldPlayedIdLike2', $oldPlayedIdLike2);
 
+            if ($development) {
+                $queryString = [
+                    $qry->queryString => [
+                        'matchId' => $matchId,
+                        'oldPlayedIdLookup' => $oldPlayedIdLookup,
+                        'newPlayerIdLookup' => $newPlayerIdLookup,
+                        'oldPlayedIdLike1' => $oldPlayedIdLike1,
+                        'oldPlayedIdLike2' => $oldPlayedIdLike2,
+                    ]
+                ];
+                $return['query_string'][] = $queryString;
+            }
             $qry->execute();
-
         }
     }
-    break; // TODO KIRIL REMOVE
 }
-
-echo 'END';
+if ($development) {
+    echo json_encode($return);
+} else {
+    echo 'Done';
+}
